eff83cba3e93208355bf103c3c87e9d484b0d42e
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-12 01:25:27 -0600

    Working demo signup form

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, FloatField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
@@ -21,7 +21,8 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
+    first_name = StringField("First Name", [DataRequired()])
+    last_name = StringField("Last Name", [DataRequired()])
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
+    buying_power = FloatField("Buying Power", [DataRequired()])
