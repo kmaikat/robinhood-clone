4219bf6795f22e9c1b99fb35bb3071f2634b6f38
Katherine <katherinepmai@gmail.com>
2022-12-11 21:54:01 -0600

    adjusted news model

--- app/models/news.py
+++ app/models/news.py
@@ -1,15 +1,21 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
+
 class News(db.Model):
     __tablename__ = 'news'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.Integer, primary_key = True)
-    like = db.Column(db.Boolean)
-    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    like = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('users.id')), nullable=False)
+    title = db.Column(db.String, nullable=False)
+    source = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    ticker = db.Column(db.String)
     article_link = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
