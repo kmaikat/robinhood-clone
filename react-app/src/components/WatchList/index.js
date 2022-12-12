8cc54c20b79d3d7a0dc21f987de3a845b3303f97
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-12-11 18:09:39 -0800

    create watchlist action types & watchlist component

---
+++ react-app/src/components/WatchList/index.js
@@ -0,0 +1,17 @@
+import * as watchlistAction from '../../store/watchlist'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+const WatchList = () => {
+
+    return (
+        <div>
+            <div>
+                <div>Lists</div>
+                <div>Plus button</div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default WatchList
---
+++ react-app/src/store/watchlist.js
@@ -0,0 +1,102 @@
+import { csrfFetch } from './csrf';
+//action types 
+const LOAD_WATCHLISTS = "watchlist/loadWatchlists";
+const ADD_WATCHLIST = "watchlist/addWatchlist"; 
+const REMOVE_WATCHLIST = "watchlist/removeWatchlist";
+const EDIT_WATCHLIST = "watchlist/editWatchlist";
+const ADD_STOCK = "watchlist/addStock";
+const REMOVE_STOCK = "watchlist/removeStock";
+
+export function loadWatchlists(watchlists) {
+    return {
+        type: LOAD_WATCHLISTS, 
+        watchlists
+    }
+}
+
+export function addWatchlist(watchlist) {
+    return {
+        type: ADD_WATCHLIST, 
+        watchlist
+    }
+}
+
+export function removeWatchlist(watchlistId) {
+    return {
+        type: REMOVE_WATCHLIST, 
+        watchlistId
+    }
+}
+
+export function editWatchlist(watchlist) {
+    return {
+        type: EDIT_WATCHLIST, 
+        watchlist
+    }
+}
+
+export function addStock(stock) {
+    return {
+        type: ADD_STOCK, 
+        stock
+    }
+}
+
+export function deleteStock(stockId) {
+    return {
+        type: REMOVE_STOCK,
+        stockId
+    }
+}
+
+export const fetchUserWatchlists = () => async dispatch => {
+    const response = await fetch(`api/watchlists/current`); 
+   
+    if (response.ok) {
+        const watchlists = await response.json();
+        dispatch(loadWatchlists(watchlists)); 
+        return response
+    }
+    
+}
+
+export const createWatchlist = (watchlist) => async dispatch => {
+    const { name } = watchlist; 
+    const response = await csrfFetch(`/api/watchlists/`, {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({name})
+    })
+
+    if (response.ok) {
+        const data = await response.json()
+        dispatch(addWatchlist(data));
+        return response
+    } else if (response.status < 500) {
+        const data = await response.json()
+        if (data.errors) {
+            return data.errors
+        }
+    } else {
+        return ['An error occured.Please try again.']
+    }
+}
+
+
+export const watchlistReducer = (state={}, action) => {
+    switch (action.type) {
+        case LOAD_WATCHLISTS: 
+            return {
+                ...state, 
+                watchlists: action.watchlists.reduct((acc, watchlist) => {
+                    acc[watchlist.id] = watchlist;
+                    return acc
+                }, {})
+            }
+        
+        default: 
+            return state
+    }
+}
