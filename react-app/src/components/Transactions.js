be7c2913c6e8f2f25e1a0f68fef1c7b3dec2090e
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-13 17:32:31 -0600

    Basic transaction form skeleton BUY

--- react-app/src/components/Transactions.js
+++ react-app/src/components/Transactions.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import "../stylesheets/Transactions.css";
+
 function Transactions() {
+    const usDollar = Intl.NumberFormat("en-US");
+    const [color, setColor] = useState("#ec5e2a");
+    const buyingPower = useSelector(state => usDollar.format(state.session.user.buyingPower));
+    const [buyOrSale, setBuyOrSale] = useState("buy");
+    const { symbol } = useParams();
+    // setColor(currentPrice >= startingPrice ? '#5ac53b' : '#ec5e2a')
+
+    function submitForm(e) {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>
-                This will be feature soon!
-            </h1>
+        <div id="transaction-container">
+            <div id="transaction-heading-container">
+                {buyOrSale === "buy" ? `Buy ${symbol}` : `Sale ${symbol}`}
+            </div>
+            <form id="transaction-form">
+                <div id="transaction-order-type">
+                    
+                </div>
+                <div id="transaction-buying-option">
+
+                </div>
+                <div id="transaction-quantity">
+
+                </div>
+                <button type="submit">
+                    Submit Order
+                </button>
+            </form>
+            <div id="transaction-buying-power-container">
+                <p>{`$${buyingPower} buying power available`}</p>
+            </div>
         </div>
     );
 }
