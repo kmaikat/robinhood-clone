e32ab46e1a23beb2ce49f5d9d0fbf488f5669d1a
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-14 10:22:06 -0600

    Stopping point to present to john. Fixing form bugs

--- react-app/src/components/Transactions.js
+++ react-app/src/components/Transactions.js
@@ -2,16 +2,27 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import "../stylesheets/Transactions.css";
+import { getOneDayPrices } from "../util/util";
 
 function Transactions() {
     const usDollar = Intl.NumberFormat("en-US");
     const [color, setColor] = useState("#ec5e2a");
     const [submittingOrder, setSubmittingOrder] = useState(false);
-    const buyingPower = useSelector(state => usDollar.format(state.session.user.buyingPower));
-    const [buyOrSale, setBuyOrSale] = useState("buy");
+    const [sharesOrDollars, setSharesOrDollars] = useState("dollars");
+    const [transactionAmount, setTransactionAmount] = useState("");
+    const buyingPower = useSelector(state => state.session.user.buyingPower);
+    const sharePrice = useSelector(state => state.price.currentPrice);
+    const ownedShares = 20;
+    const [buyOrSale, setBuyOrSale] = useState("sell");
     const symbol = useParams().symbol.toUpperCase();
     // setColor(currentPrice >= startingPrice ? '#5ac53b' : '#ec5e2a')
 
+    async function tester() {
+        const test = await getOneDayPrices(symbol);
+        console.log(test)
+    }
+
+    console.log(tester());
     function submitOrder(e) {
         e.preventDefault();
     }
@@ -36,13 +47,34 @@ function Transactions() {
 
                 <form id="transaction-form" onSubmit={submitOrder}>
                     <div className="transaction-form-data-container">
-
+                        <p>Order Type</p>
+                        <p>Market Order</p>
                     </div>
                     <div className="transaction-form-data-container">
-
+                        <label>Buy In</label>
+                        <select>
+                            <option>Dollars</option>
+                            <option>Shares</option>
+                        </select>
                     </div>
-                    <div className="transaction-form-data-container">
+                    <div className="transaction-form-data-container" id="transaction-amount">
+                        <p>Amount</p>
+                        <input type="text"
+                            placeholder="$0.00"
+                            value={transactionAmount}
+                            onChange={(event) => {
+                                if (isNaN(event.target.value) === false) {
+                                    // buy conditions
+                                    if (buyOrSale === "buy" && sharesOrDollars === "dollars" && Number(buyingPower) > Number(event.target.value)) setTransactionAmount(event.target.value);
 
+                                    //sell conditions
+                                    if (sharesOrDollars === "shares" && Number(ownedShares) > Number(event.target.value)) setTransactionAmount(event.target.value);
+                                }
+                            }} />
+                    </div>
+                    <div className="transaction-form-data-container" id="transaction-est-quantity">
+                        <p>Est. Quantity</p>
+                        <p>0</p>
                     </div>
                     <div id="transaction-submit-container">
                         <button id="transaction-submit-button" type="submit" onClick={() => setSubmittingOrder(true)} className={`transaction-submit-${buyOrSale}`}>
@@ -51,8 +83,19 @@ function Transactions() {
                     </div>
                 </form>
                 {buyOrSale === "buy" && <div id="transaction-buying-power-container">
-                    <p>{`$${buyingPower} buying power available`}</p>
+                    <p>{`$${usDollar.format(buyingPower)} buying power available`}</p>
                 </div>}
+                {buyOrSale === "sell" &&
+                    // sharesOrDollars === "shares" &&
+                    <div id="transaction-buying-power-container">
+                        <p>{`${ownedShares} ${symbol} shares remaining`}</p>
+                    </div>
+                }
+                {buyOrSale === "sell" &&
+                    // sharesOrDollars === "shares" &&
+                    <div id="transaction-buying-power-container">
+                        <p>{`Roughly ${usDollar.format(Number(ownedShares) * Number(sharePrice))} of ${symbol} remaining`}</p>
+                    </div>}
             </div>
         </div>
     );
