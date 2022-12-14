d7ef4c872782160b0ae1627746f0ff19982f1bdd
Katherine <katherinepmai@gmail.com>
2022-12-14 14:49:33 -0600

    migrated and seed

--- migrations/versions/20221214_144820_.py
+++ migrations/versions/20221214_144820_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 55c2e29e4c2a
+Revision ID: 6c656ac4571c
 Revises: 
-Create Date: 2022-12-14 10:37:48.796797
+Create Date: 2022-12-14 14:48:20.264777
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '55c2e29e4c2a'
+revision = '6c656ac4571c'
 down_revision = None
 branch_labels = None
 depends_on = None
