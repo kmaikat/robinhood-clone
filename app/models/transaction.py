0c868e83621a2954c57ed3248af9f2b2edafb1d3
Katherine <katherinepmai@gmail.com>
2022-12-14 16:12:23 -0600

    update seed order

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -9,7 +9,6 @@ class Transaction(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    stock_symbol = db.Column(db.String(10), db.ForeignKey(add_prefix_for_prod('assets.symbol')), nullable=False)
     transaction_type = db.Column(db.String(20), nullable=False) # buying / selling ?
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Float, nullable=False)
@@ -17,5 +16,4 @@ class Transaction(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    asset = db.relationship(add_prefix_for_prod('Asset'), back_populates='transactions')
     user = db.relationship(add_prefix_for_prod('User'), back_populates='transactions')
