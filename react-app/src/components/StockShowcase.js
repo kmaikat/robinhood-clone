9d9e679e203ab5fdf2620a0ef48405f1ae968757
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-15 16:30:18 -0600

    Final touches

--- react-app/src/components/StockShowcase.js
+++ react-app/src/components/StockShowcase.js
@@ -37,14 +37,14 @@ function StockShowcase() {
             });
     }, [symbol]);
 
-    // useEffect(() => {
-    //     fetch(`/api/stock/company-information/${symbol}`)
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             setCompanyInfo(res);
-    //             setCompanyInfoLoaded(true);
-    //         });
-    // }, [symbol]);
+    useEffect(() => {
+        fetch(`/api/stock/company-information/${symbol}`)
+            .then(res => res.json())
+            .then(res => {
+                setCompanyInfo(res);
+                setCompanyInfoLoaded(true);
+            });
+    }, [symbol]);
 
     console.log(companyInfo);
     return (
@@ -175,7 +175,7 @@ function StockShowcase() {
                             </div>
                         </div>
                         <div className="app-stock-news-container">
-                            {/* <SymbolNews /> */}
+                            <SymbolNews />
                         </div>
                     </div>
                     <div className="stock-showcase-right">
