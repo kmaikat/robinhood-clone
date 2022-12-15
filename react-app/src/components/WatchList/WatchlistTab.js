17775f5a380899199717495be5784d926e11d1d6
Ha Nguyen <vietha.nguyen3110@gmail.com>
2022-12-15 15:51:08 -0800

    added className.

--- react-app/src/components/WatchList/WatchlistTab.js
+++ react-app/src/components/WatchList/WatchlistTab.js
@@ -20,15 +20,15 @@ const WatchlistTab = () => {
         })
     }
     return (
-        <div>
+        <div className="profile-page-watchlists-container">
             {watchlists && Object.values(watchlists).map(
                 (watchlist,i) => (
-                    <div>
-                        <button onClick={handleClick(i)}>
+                    <div className="profile-page-watchlists-label">
+                        <button onClick={handleClick(i)} className="profile-page-watchlists-btnname">
                             <span>{watchlist.name}</span>
                         </button>
                         {isOpen[i] && 
-                            <div>
+                            <div className="profile-page-watchlists-stocks">
                                 {watchlist.watchlist_stocks.length > 0 && 
                                     watchlist.watchlist_stocks.map(
                                         stock => (
@@ -39,7 +39,7 @@ const WatchlistTab = () => {
                                     )
                                 }
                                 {watchlist.watchlist_stocks.length === 0 && 
-                                    <div>
+                                    <div className="profile-page-watchlists-nostock">
                                         <h1> Feels a little empty here...</h1>
                                         <p> Search for companies to add and stay up to date</p>
                                     </div>
