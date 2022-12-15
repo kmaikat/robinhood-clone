d81087b4581a17d3fd251c32985e9e83166c47c3
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-14 18:55:16 -0600

    Fix search for psql

--- app/api/stock_routes.py
+++ app/api/stock_routes.py
@@ -16,7 +16,7 @@ def index():
 
 @stock_routes.route('/search/<string:keyword>')
 def search_symbols(keyword):
-    result = [{'symbol': item.stock_symbol, 'name': item.company} for item in StockSymbol.query.filter(StockSymbol.stock_symbol.like(f'%{keyword}%') | StockSymbol.company.like(
+    result = [{'symbol': item.stock_symbol, 'name': item.company} for item in StockSymbol.query.filter(StockSymbol.stock_symbol.ilike(f'%{keyword}%') | StockSymbol.company.ilike(
         f'%{keyword}%')).order_by(case((StockSymbol.stock_symbol.startswith(keyword), 0), (StockSymbol.company.startswith(keyword), 1), else_=2)).limit(7)]
 
     return jsonify(result)
