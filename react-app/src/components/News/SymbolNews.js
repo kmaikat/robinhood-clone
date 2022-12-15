23ade23a78af6a5d41771f88cb1b675ec6f26723
Katherine <katherinepmai@gmail.com>
2022-12-15 14:10:40 -0600

    added like button to symbol stock news

--- react-app/src/components/News/SymbolNews.js
+++ react-app/src/components/News/SymbolNews.js
@@ -1,13 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import { addArticleToDb, deleteArticleFromDb, getArticlesFromDb } from "../../store/news";
 import "../../stylesheets/AllNews.css";
 import "../../stylesheets/SymbolNews.css";
 
 const SymbolNews = () => {
     const [articles, setArticles] = useState([]);
+    const [isLiked, setIsLiked] = useState(false)
+    const currentUser = useSelector(state => state.session.user)
+    const likedArticles = useSelector(state => state.news)
+    const dispatch = useDispatch()
     const { symbol } = useParams();
 
+    const handleAddToggle = (e, article) => {
+        e.preventDefault()
+        e.stopPropagation()
+        const submission = {
+            "user_id": currentUser.id,
+            "title": article.title,
+            "source": article.source,
+            "image": article.image,
+            "article_link": article.url
+        }
+        dispatch(addArticleToDb(submission))
+        setIsLiked(!isLiked)
+    }
+
+    const handleDeleteToggle = (e, article) => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const likedArticle = likedArticles[article.url]
+        dispatch(deleteArticleFromDb(likedArticle))
+        setIsLiked(!isLiked)
+    }
+
     useEffect(() => {
+        dispatch(getArticlesFromDb())
         setArticles([])
         fetch(`/api/news/${symbol}`).then(r => r.json()).then(r => setArticles(r));
     }, [symbol]);
@@ -26,7 +56,7 @@ const SymbolNews = () => {
                                     <div id="all-news-source">
                                         {article.source}
                                     </div>
-                                    <div id="all-news-bottom-half">
+                                    <div id="all-news-bottom-half" className="symbol-news-bottom-half">
                                         <div id="all-news-title-and-tickers">
                                             <div id="all-news-article-title">
                                                 {article.title}
@@ -37,6 +67,9 @@ const SymbolNews = () => {
                                 <div className="stock-news-image-container">
                                     <img src={article.image} />
                                 </div>
+                                        <div id="all-news-article-like">
+                                            {article.url in likedArticles ? <i className="fa-solid fa-bookmark" id="news-saved" onClick={e => handleDeleteToggle(e, article)} /> : <i className="fa-regular fa-bookmark" id="news-not-saved" onClick={e => handleAddToggle(e, article)} />}
+                                        </div>
                             </div>
                         </div>
                     </a>
