fe85a55e9f39b25e93c3546a437b088ee6775917
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-14 19:10:54 -0600

    Successfully seed data into postgres

--- app/models/stock_symbol.py
+++ app/models/stock_symbol.py
@@ -3,6 +3,9 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class StockSymbol(db.Model):
     __tablename__ = 'stock_symbols'
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+        
     id = db.Column(db.Integer, primary_key=True)
     stock_symbol = db.Column(db.String(10), nullable=False)
     company = db.Column(db.String, nullable=False)
