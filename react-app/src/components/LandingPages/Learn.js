e4c06139f67633a7d187733191187089d73a5169
Katherine <katherinepmai@gmail.com>
2022-12-16 01:21:07 -0600

    header with image nearly placed properly

--- react-app/src/components/LandingPages/Learn.js
+++ react-app/src/components/LandingPages/Learn.js
@@ -7,11 +7,13 @@ const LearnPage = () => {
     return (
         <div id="learn-page-body">
             {/* <div id="learn-page-inventing-basics-container"> */}
-                <div id="learn-page-inventing-basics-heading-container">
-                    <div id="learn-page-inventing-basic-title">
-                        Inventing basics
-                    </div>
+            <div id="learn-page-inventing-basics-heading-container">
+                <div id="learn-page-inventing-basic-title">
+                    Inventing basics
                 </div>
+            </div>
+            <div id="learn-page-inventing-body-container">
+
                 <div id="learn-page-inventing-basics-text-image-container">
                     <div id="inventing-basics-text-block">
                         <div id="inventing-basics-text">
@@ -23,14 +25,17 @@ const LearnPage = () => {
                             </div>
 
                         </div>
-
                     </div>
                     <div id="inventing-basics-image-block">
                         <img className="hero-journey-img" src={heroJourney} alt="hero-journey-pic" />
                     </div>
 
                 </div>
-            {/* </div> */}
+            </div>
+
+            {/* investing 101 */}
+
+
         </div>
     )
 
