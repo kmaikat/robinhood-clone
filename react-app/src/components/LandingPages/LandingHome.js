7aba81df2198370cd7ca38a95dc12f4eb848697d
Katherine <katherinepmai@gmail.com>
2022-12-16 16:27:08 -0600

    centered text under video on home page

--- react-app/src/components/LandingPages/LandingHome.js
+++ react-app/src/components/LandingPages/LandingHome.js
@@ -5,12 +5,23 @@ const LandingHome = () => {
     return (
         <div id='home-whole-container'>
             <div id="home-video-section">
-                <video className="video" controlsList="nodownload nofullscreen noremoteplayback" autoPlay playsInline muted>
-                    <source media="(min-width:1400px)" src="https://rockethood.s3.us-west-2.amazonaws.com/assets/retirement-hero-hq__67df1aeb147a73f52166e1f391f37f0e.mp4" type="video/mp4" />
-                </video>
+                <div id="video-container">
+
+                    <video className="video" controlsList="nodownload nofullscreen noremoteplayback" autoPlay playsInline muted>
+                        <source media="(min-width:1400px)" src="https://rockethood.s3.us-west-2.amazonaws.com/assets/retirement-hero-hq__67df1aeb147a73f52166e1f391f37f0e.mp4" type="video/mp4" />
+                    </video>
+                </div>
                 <div id="under-video-container">
                     <div id="under-video-text">
-                        Earn a 1% match. No employer necessary.
+                        <div id="top-line">
+                            Earn a 1% match.
+                        </div>
+                        <div id="bottom-line">
+                            No employer necessary.
+                        </div>
+                    </div>
+                    <div id="button-container">
+                        container
                     </div>
                 </div>
             </div>
