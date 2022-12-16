4a5c6114ca1526da5b2c3efc5e59764baeaadff6
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-16 13:30:59 -0600

    Fix unique constraint

--- app/models/assets.py
+++ app/models/assets.py
@@ -5,20 +5,27 @@ from datetime import datetime
 class Asset(db.Model):
     __tablename__ = "assets"
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
-
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer,
                         db.ForeignKey(add_prefix_for_prod("users.id")),
                         nullable=False)
-    symbol = db.Column(db.String, nullable=False, unique=True)
+    symbol = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     quantity = db.Column(db.Float, nullable=False)
     avg_price = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
     user = db.relationship("User", back_populates="assets")
+
+    if environment == "production":
+        __table_args__ = (
+            db.UniqueConstraint("user_id", "symbol"), {
+                "schema": SCHEMA,
+            })
+    else:
+        __table_args__ = (
+            db.UniqueConstraint("user_id", "symbol"),
+        )
 
     def to_dict(self):
         return {
