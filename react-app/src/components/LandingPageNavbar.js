e6e01e6bf1aa79990ba69ec0bf7e72d614effc94
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2022-12-15 18:33:17 -0600

    More mobile stuff

--- react-app/src/components/LandingPageNavbar.js
+++ react-app/src/components/LandingPageNavbar.js
@@ -12,42 +12,44 @@ const LandingPageNavbar = () => {
     }
 
     return (
-        <div className={`landing-page ${current_url === "/" ? "landing-page-green" : "landing-page-white"}`}>
-            <NavLink exact to="/">
-                <div className="landing-page-logo">
-                    <p className="landing-page-logo-text">Rockethood</p>
-                    <i className="fa-solid fa-rocket"></i>
+        <div className="landing-page-container">
+            <div className={`landing-page ${current_url === "/" ? "landing-page-green" : "landing-page-white"}`}>
+                <NavLink exact to="/">
+                    <div className="landing-page-logo">
+                        <p className="landing-page-logo-text">Rockethood</p>
+                        <i className="fa-solid fa-rocket"></i>
+                    </div>
+                </NavLink>
+                <div className="landing-page-navlink-actions-container">
+                    <ul className="landing-page-navlinks">
+                        <NavLink to="/invest">
+                            <li className="landing-page-navlink" id="landing-page-Invest-button">Invest</li>
+                        </NavLink>
+                        <NavLink to="/crypto">
+                            <li className="landing-page-navlink" id="landing-page-Crypto-button">Crypto</li>
+                        </NavLink>
+                        <NavLink to="/learn">
+                            <li className="landing-page-navlink" id="landing-page-learn-button">Learn</li>
+                        </NavLink>
+                        <NavLink to="/snacks">
+                            <li className="landing-page-navlink" id="landing-page-Snacks-button">Snacks</li>
+                        </NavLink>
+                    </ul>
+                    <div className="landing-page-actions">
+                        <NavLink to="/login" exact>
+                            <button id="landing-page-login">
+                                Log in
+                            </button>
+                        </NavLink>
+                        <NavLink to="/sign-up" exact>
+                            <button id="landing-page-signup">
+                                Sign up
+                            </button>
+                        </NavLink>
+                    </div>
                 </div>
-            </NavLink>
-            <div className="landing-page-navlink-actions-container">
-                <ul className="landing-page-navlinks">
-                    <NavLink to="/invest">
-                        <li className="landing-page-navlink" id="landing-page-Invest-button">Invest</li>
-                    </NavLink>
-                    <NavLink to="/crypto">
-                        <li className="landing-page-navlink" id="landing-page-Crypto-button">Crypto</li>
-                    </NavLink>
-                    <NavLink to="/learn">
-                        <li className="landing-page-navlink" id="landing-page-learn-button">Learn</li>
-                    </NavLink>
-                    <NavLink to="/snacks">
-                        <li className="landing-page-navlink" id="landing-page-Snacks-button">Snacks</li>
-                    </NavLink>
-                </ul>
-                <div className="landing-page-actions">
-                    <NavLink to="/login" exact>
-                        <button id="landing-page-login">
-                            Log in
-                        </button>
-                    </NavLink>
-                    <NavLink to="/sign-up" exact>
-                        <button id="landing-page-signup">
-                            Sign up
-                        </button>
-                    </NavLink>
-                </div>
-            </div>
-        </div >
+            </div >
+        </div>
     );
 };
 
