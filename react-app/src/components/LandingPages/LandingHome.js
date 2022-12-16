ac8a9b2823e0a2ab8b4777318b42d91bbbc4b5dc
Katherine <katherinepmai@gmail.com>
2022-12-16 14:56:06 -0600

    added text under te video

--- react-app/src/components/LandingPages/LandingHome.js
+++ react-app/src/components/LandingPages/LandingHome.js
@@ -8,6 +8,11 @@ const LandingHome = () => {
                 <video className="video" controlsList="nodownload nofullscreen noremoteplayback" autoPlay playsInline muted>
                     <source media="(min-width:1400px)" src="https://rockethood.s3.us-west-2.amazonaws.com/assets/retirement-hero-hq__67df1aeb147a73f52166e1f391f37f0e.mp4" type="video/mp4" />
                 </video>
+                <div id="under-video-container">
+                    <div id="under-video-text">
+                        Earn a 1% match. No employer necessary.
+                    </div>
+                </div>
             </div>
         </div>
     )
